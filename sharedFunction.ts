2384a384600072a91975b77d0bc3aad906ed7d99
Udupa, Prashanth <prashanth.udupa@fmr.com>
2016-01-28 18:22:50 +0530

    Use of Modules to share functions between files.

---
+++ sharedFunction.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by a487037 on 01/28/2016.
+ */
+module Utility {
+    export class Useful {
+        timesTwo(n:number):number {
+            return n * 2;
+        }
+    }
+}
---
+++ util.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by a487037 on 01/28/2016.
+ */
+///<reference path='sharedFunction.ts'/>
+var use = new Utility.Useful();
+console.log(use.timesTwo(4));
