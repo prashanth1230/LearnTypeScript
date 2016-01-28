0868e716b1f22999ab9504a8b2a7f4dace794469
Udupa, Prashanth <prashanth.udupa@fmr.com>
2016-01-28 15:45:55 +0530

    Added more types examples.

--- types.ts
+++ types.ts
@@ -1,9 +1,19 @@
 /**
  * Created by a487037 on 01/28/2016.
  */
-var n : Number = 1;
+var n:Number = 1;
 
-var s : String = 'abc';
+var s:String = 'abc';
 
-var a : any = 1;
+var a:any = 1;
 a = 'abc';
+
+var names:string[] = ['John', 'Arya', 'Prashanth'];
+
+enum Starks {Jon, Brad, Eddn, Cathlyn}
+
+var cat:Starks = Starks.Eddn;
+
+function getName():String {
+    return 'John';
+}
